cff911c78eed5aada2661820d4743987458a721c
23000633g <156690386+23000633g@users.noreply.github.com>
2024-01-22 12:32:39 +0000

    Add random joke feature to homepage

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html", title="Hello")
+    response = requests.get("https://official-joke-api.appspot.com/jokes/random")
+    joke = response.json()
+    setup = joke["setup"]
+    punchline = joke["punchline"]
+    return render_template("index.html", title="Hello", setup=setup, punchline=punchline)
